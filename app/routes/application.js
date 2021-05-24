3942c67ab3a71eaf7a9ff788f0a3b9b59f759ead
Bartlomiej Dudzik <bartholomew.dudzik@gmail.com>
2021-05-24 15:00:31 +0200

    feat: Use ember-simple-auth

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,18 +3,10 @@ import { inject as service } from "@ember/service";
 
 export default class ApplicationRoute extends Route {
   @service intl;
-  @service auth;
   @service router;
 
-  beforeModel(transition) {
-    super.beforeModel(transition);
-
-    if (this.auth.isAuthenticated) {
-      this.router.transitionTo("index");
-    } else {
-      this.router.transitionTo("auth");
-    }
-
+  beforeModel() {
+    super.beforeModel(...arguments);
     this.intl.setLocale(["en-us"]);
   }
 }
--- tests/acceptance/authentication-test.js
+++ tests/acceptance/authentication-test.js
@@ -2,22 +2,15 @@ import { module, test } from "qunit";
 import { visit, currentURL, fillIn, click } from "@ember/test-helpers";
 import { setupApplicationTest } from "ember-qunit";
 import { setupPretender } from "ucentral/tests/helpers/pretender";
-import UcentralRouterAuthenticator from "ucentral/authenticators/ucentral-router";
-
-const createAuthServiceStub = (loginStub) =>
-  class UcentralRouterAuthenticatorStub extends UcentralRouterAuthenticator {
-    authenticate = loginStub;
-  };
+import { invalidateSession } from "ember-simple-auth/test-support";
 
 module("Acceptance | authentication", function (hooks) {
   setupApplicationTest(hooks);
   setupPretender(hooks);
 
   test("user is redirected to /auth when not authenticated", async function (assert) {
-    this.pretender.post("/authenticate", () => {
-      assert.ok(true, "successfuly hit authenticate endpoint");
-      return [200, {}, "{}"];
-    });
+    await invalidateSession();
+
     await visit("/");
 
     assert.equal(currentURL(), "/auth");
@@ -25,10 +18,8 @@ module("Acceptance | authentication", function (hooks) {
 
   test("given user entered correct credentials they are redirected to /dashboard", async function (assert) {
     this.pretender.post("/authenticate", () => {
-      return [200, {}, "{}"];
+      return [200, {}, JSON.stringify({ succeeded: true })];
     });
-    const authStub = createAuthServiceStub(async () => true);
-    this.owner.register("service:auth", authStub);
     await visit("/");
 
     await fillIn("[data-test-ip-address] [data-test-input]", "192.168.1.2");
@@ -41,8 +32,22 @@ module("Acceptance | authentication", function (hooks) {
   });
 
   test("given user entered incorrect credentials they are not redirected", async function (assert) {
-    const authStub = createAuthServiceStub(async () => false);
-    this.owner.register("service:auth", authStub);
+    this.pretender.post("/authenticate", () => {
+      return [400, {}, "{}"];
+    });
+
+    await visit("/");
+
+    await fillIn("[data-test-ip-address] [data-test-input]", "192.168.1.2");
+    await click("[data-test-confirm-button]");
+
+    await fillIn("[data-test-password] [data-test-password-input]", "Secret");
+    await click("[data-test-confirm-button]");
+
+    assert.equal(currentURL(), "/auth");
+  });
+
+  test("given authenticate endpoint failed to respond", async function (assert) {
     await visit("/");
 
     await fillIn("[data-test-ip-address] [data-test-input]", "192.168.1.2");
