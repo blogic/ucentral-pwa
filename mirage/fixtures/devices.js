bf84f0bb782161fbb27a835e1ec372081e96686f
Bartlomiej Dudzik <bartholomew.dudzik@gmail.com>
2021-06-14 08:13:14 +0200

    feat: create Uc::DeviceWithStatus component

---
+++ mirage/fixtures/devices.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    UUID: "1-1",
+    serialNumber: "A-A",
+    deviceType: "AP",
+    macAddress: "00:1A:C1",
+    location: "entrance",
+    connectedDevicesCount: 13,
+    health: 85,
+  },
+  {
+    UUID: "2-2",
+    serialNumber: "B-B",
+    deviceType: "AP",
+    macAddress: "00:1A:C2",
+    location: "meeting",
+    connectedDevicesCount: 3,
+    health: 55,
+  },
+  {
+    UUID: "3-3",
+    serialNumber: "C-C",
+    deviceType: "SWITCH",
+    macAddress: "00:1A:C3",
+    location: "Office 1",
+    connectedDevicesCount: 0,
+    health: 15,
+  },
+];
